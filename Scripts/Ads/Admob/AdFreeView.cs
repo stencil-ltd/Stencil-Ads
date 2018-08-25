58cffe588c8e4819c5f8f74e3977c47c150d844d
Aaron Sarazan <aaron@sarazan.net>
2018-08-25 14:49:02 -0700

    stencil-ads first check-in

---
+++ Scripts/Ads/Admob/AdFreeView.cs
@@ -0,0 +1,23 @@
+﻿#if STENCIL_ADMOB
+using UnityEngine;
+
+namespace Ads.Admob
+{
+    public class AdFreeView : MonoBehaviour
+    {
+        public static int Count { get; private set; }
+
+        private void OnEnable()
+        {
+            Count++;
+            AdmobBannerArea.HideBanner();
+        }
+
+        private void OnDisable()
+        {
+            if (--Count == 0)
+                AdmobBannerArea.ShowBanner();
+        }
+    }
+}
+#endif
---
+++ Scripts/Ads/CrossPromoController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Util;
+
+namespace Ads
+{
+    public class CrossPromoController : MonoBehaviour
+    {
+        public float LoadTime = 2f;
+        public AdSettings Settings; // prevent build-time culling.
+        private AsyncOperation _scene;
+
+        private void Awake()
+        {
+            StencilAds.InitHouse();
+            StencilAds.House.OnError += OnError;
+            StencilAds.House.OnComplete += OnComplete;
+            new GameObject("Main Thread").AddComponent<UnityMainThreadDispatcher>();
+        }
+
+        private IEnumerator Start()
+        {
+            yield return null;
+            _scene = SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+            _scene.allowSceneActivation = false;
+        
+            yield return new WaitForSeconds(LoadTime);
+            if (StencilAds.House.IsReady)
+            {
+                Debug.Log("Found House Ad. Showing.");
+                StencilAds.House.Show();
+            }
+            else
+            {
+                Debug.Log("No House Ads. Moving On.");
+                Continue();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            StencilAds.House.OnError -= OnError;
+            StencilAds.House.OnComplete -= OnComplete;
+        }
+
+        private void Continue()
+        {
+            _scene.allowSceneActivation = true;
+            if (_scene.isDone)
+                SceneManager.UnloadSceneAsync("Startup");
+            else
+                _scene.completed += operation => SceneManager.UnloadSceneAsync("Startup");
+        }
+
+        private void OnComplete(object sender, EventArgs e)
+        {
+            Debug.Log("Loading Game...");
+            Continue();
+        }
+
+        private void OnError(object sender, EventArgs e)
+        {
+            Debug.LogWarning("House Ad Error");
+            Continue();
+        }
+    }
+}
