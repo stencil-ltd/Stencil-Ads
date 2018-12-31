f436169c32a8e85f0105c048d1f20c48785f619f
Aaron Sarazan <aaron@sarazan.net>
2018-12-30 20:45:58 -0800

    basic rating

---
+++ Scripts/Ratings/BasicRatingController.cs
@@ -0,0 +1,61 @@
+using System;
+using PaperPlaneTools;
+using Scripts.RemoteConfig;
+using UI;
+using UnityEngine;
+
+namespace Ratings
+{
+    public class BasicRatingController : Controller<BasicRatingController>
+    {
+        public float delayShow = 1f;
+        public bool checkAtAwake = true;
+        
+        public override void Register()
+        {
+            base.Register();
+            RateSettings.Instance.BindRemoteConfig();
+        }
+        
+
+        private void OnEnable()
+        {
+            if (!StencilRateHelpers.IsEnabled)
+            {
+                Debug.LogWarning("Rating is not enabled...");
+                enabled = false;
+                return;
+            }
+            
+            if (checkAtAwake)
+                Invoke(nameof(Check), Math.Max(0.1f, delayShow));
+        }
+
+        public bool Check()
+        {
+            if (!RateSettings.Instance.Config.CheckConditions()) return false;
+            ForceShow();
+            return true;
+        }
+        
+        public void ForceShow()
+        {
+            StencilRateHelpers.MarkShown();
+            #if UNITY_IOS
+            StencilRateHelpers.NativeRate();
+            #else
+            AlertReview();
+            #endif
+        }
+
+        private void AlertReview()
+        {
+            var config = RateSettings.Instance.Config;
+            new Alert($"Enjoying {Application.productName}?", "Would you mind leaving us a review?")
+                .SetNegativeButton("Never", StencilRateHelpers.Reject)
+                .SetNeutralButton("Maybe Later")
+                .SetPositiveButton("OK!", () => { config.GoToRateUrl(); })
+                .Show();
+        }
+    }
+}
